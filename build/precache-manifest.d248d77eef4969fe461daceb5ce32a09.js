d6653850c78b0ad2cb9bc8698f7d0e3fa3239c4f
undacat <shannon@ruder.net>
2019-05-04 10:34:53 -0700

    reorganized file structure

---
+++ build/precache-manifest.d248d77eef4969fe461daceb5ce32a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50591960b3268252cf47888ecc9a495",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "f7119c43b47d50b605b2",
+    "url": "/react-memory-game/static/css/main.d812ee1f.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/react-memory-game/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "f7119c43b47d50b605b2",
+    "url": "/react-memory-game/static/js/main.d853093b.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
